64a2a6f43c5e8912a8db7deb59785115d0a94c3a
최한준 <choihanjun@choehanjun-ui-MacBookAir.local>
2023-11-14 12:35:39 +0000

    ADD UNET Base Architecture

--- DDPM_from_scatch/sd/diffusion.py
+++ DDPM_from_scatch/sd/diffusion.py
@@ -18,6 +18,90 @@ class TimeEmbedding(nn.Module):
         # (1, 1280)
         return x
 
+class SwitchSequential(nn.Sequential):
+    def forward(self, x : torch.Tensor, context: torch.Tensor, time: torch.Tensor) -> torch.Tensor:
+        for layer in self:
+            if isinstance(layer, UNET_AttentionBlock):
+                x = layer(x, context) # compute the cross attention between our latents and the prompt
+            elif isinstance(layer, UNET_ResidualBlock):
+                x = layer(x, time)
+            else:
+                x = layer(x)
+        return x
+            
+
+def UNET(nn.Module):
+    
+    def __init__(self):
+        
+        self.encoders = nn.Module([
+            # (Batch_Size, 4, Height / 8, Width / 8) -> # (Batch_Size, 320, Height / 8, Width / 8)
+            SwitchSequential(nn.Conv2d(4, 320, kernel_size=3, padding=1)),
+            
+            SwitchSequential(UNET_residualBlock(320, 320), UNET_AttentionBlock(8, 40)),
+            
+            SwitchSequential(UNET_residualBlock(320, 320), UNET_AttentionBlock(8, 40)),
+            
+            # (Batch_Size, 320, Height / 8, Width / 8) -> # (Batch_Size, 320, Height / 16, Width / 16)
+            SwitchSequential(nn.Conv2d(320, 320, kernel_size=3, stride=2, padding=1)),
+            
+            SwitchSequential(UNET_residualBlock(320, 640), UNET_AttentionBlock(8, 80)),
+            
+            SwitchSequential(UNET_residualBlock(640, 640), UNET_AttentionBlock(8, 80)),
+            
+            # (Batch_Size, 640, Height / 16, Width / 16) -> # (Batch_Size, 640, Height / 32, Width / 32)
+            SwitchSequential(nn.Conv2d(640, 640, kernel_size=3, stride=2, padding=1)),
+            
+            SwitchSequential(UNET_residualBlock(640, 1280), UNET_AttentionBlock(8, 160)),
+            
+            SwitchSequential(UNET_residualBlock(1280, 1280), UNET_AttentionBlock(8, 160)),
+            
+            # (Batch_Size, 1280, Height / 32, Width / 32) -> (Batch_Size, 1280, Height / 64, Width / 64)
+            SwitchSequential(nn.Conv2d(1280, 1280, kernel_size=3, stride=2, padding=1)),
+            
+            # (Batch_Size, 1280, Height / 64, Width / 64) -> (Batch_Size, 1280, Height / 64, Width / 64)
+            SwitchSequential(UNET_residualBlock(1280, 1280)),
+            
+            # (Batch_Size, 1280, Height / 64, Width / 64) -> (Batch_Size, 1280, Height / 64, Width / 64)
+            SwitchSequential(UNET_residualBlock(1280, 1280)),
+        ])
+        
+        self.bottleneck = SwitchSequential(
+            UNET_ResidualBlock(1280, 1280),
+            
+            UNET_AttentionBlock(8, 160),
+            
+            UNET_ResidualBlock(1280, 1280),
+        )
+        
+        self.decoderrs = nn.ModuleList([
+            # (Batch_Size, 2560, Height / 64, Width / 64) -> (Batch_Size, 1280, Height / 64, Width / 64)
+            SwitchSequential(UNET_residualBlock(2560, 1280)),
+            
+            SwitchSequential(UNET_residualBlock(2560, 1280)),
+            
+            SwitchSequential(UNET_residualBlock(2560, 1280), Upsample(1280)),
+            
+            SwitchSequential(UNET_residualBlock(2560, 1280), UNET_AttentionBlock(8, 160)),
+            
+            SwitchSequential(UNET_residualBlock(1920, 1280), UNET_AttentionBlock(8, 160), Upsample(1280)),
+            
+            SwitchSequential(UNET_residualBlock(1920, 640), UNET_AttentionBlock(8, 80)),
+            
+            SwitchSequential(UNET_residualBlock(1280, 640), UNET_AttentionBlock(8, 80)),
+            
+            SwitchSequential(UNET_residualBlock(960, 640), UNET_AttentionBlock(8, 80), Upsample(640)),
+            
+            SwitchSequential(UNET_residualBlock(960, 320), UNET_AttentionBlock(8, 40)),
+            
+            SwitchSequential(UNET_residualBlock(640, 320), UNET_AttentionBlock(8, 40)),
+            
+            # (Batch, 320, Height / 8, Width / 8)
+            SwitchSequential(UNET_residualBlock(640, 320), UNET_AttentionBlock(8, 40)),
+        ])
+        
+        
+
 class Diffusion(nn.Module):
     
     def __init__(self):
