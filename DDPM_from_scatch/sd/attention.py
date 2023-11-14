0e3aa3cc83fa34f9bf8e1c97e7b48cfc6fad7e5a
최한준 <choihanjun@choehanjun-ui-MacBookAir.local>
2023-11-14 16:54:53 +0000

    Add CrossAttention Module

--- DDPM_from_scatch/sd/attention.py
+++ DDPM_from_scatch/sd/attention.py
@@ -54,4 +54,47 @@ class SelfAttention(nn.Module):
         # (Batch_Size, Seq_Len, Dim)
         return output
         
+class CrossAttention(nn.Module):
+    
+    def __init__(self, n_heads: int, d_embed: int, d_cross: int, in_proj_bias=True, out_proj_bias=True):
+        super().__init__()
+        self.q_proj = nn.Linear(d_embed, d_embed, bias=in_proj_bias)
+        self.k_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+        self.v_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+        self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+        self.n_heads = n_heads
+        self.d_head = d_embed // n_heads
+    
+    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        # x : (latent): (Batch_Size, Seq_Len_Q, Dim_Q)
+        # y : (contet) : (Batch_Size, Seq_Len_KV, Dim_KV) = (Batch_Size, 77, 768)
         
+        input_shape = x.shape
+        batch_Size, sequence_length, d_embed = input_shape
+        
+        interim_shape = (batch_Size, -1, self.n_heads, self.d_head)
+        
+        # Multiply query by Wq
+        q = self.q_proj(x)
+        k = self.k_proj(y)
+        v = self.v_proj(y)
+        
+        q = q.view(interim_shape).transpose(1, 2)
+        k = k.view(interim_shape).transpose(1, 2)
+        v = v.view(interim_shape).transpose(1, 2)
+        
+        weight = q @ k.transpose(-1, -2)
+        weight /= math.sqert(self.d_head)
+        
+        # 여기서는 Masking과정을 하지 않는데, 그 이유는 단순하게도 image와 prompt간의 attention을 계산하기에, next word를 보면 안되는 그럼 개념이 없다.
+        weight = F.softmax(weight, dim = -1)
+        
+        output = weight @ v
+        
+        output = output.transpose(1, 2).contiguous()
+        
+        output = output.view(input_shape)
+        
+        output = self.out_proj(output)
+        
+        return output
