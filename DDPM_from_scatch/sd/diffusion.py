76f7eb48c8d6fb4fccb437bb9e9e60c6b2c2012c
최한준 <choihanjun@choehanjun-ui-MacBookAir.local>
2023-11-14 12:38:15 +0000

    Add Upsample Module

--- DDPM_from_scatch/sd/diffusion.py
+++ DDPM_from_scatch/sd/diffusion.py
@@ -29,9 +29,19 @@ class SwitchSequential(nn.Sequential):
                 x = layer(x)
         return x
             
-
-def UNET(nn.Module):
+class Upsample(nn.Module):
     
+    def __init__(self, channels: int):
+        super().__init__()
+        self.conv = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+        
+    def forward(self, x):
+        # (Batch_Size, Features, Height, Width) ->  (Batch_Size, Features, Height * 2, Width * 2)
+        x = F.interpolate(x, scale_factor=2, mode="nearest")
+        return self.conv(x)
+        
+
+class UNET(nn.Module):
     def __init__(self):
         
         self.encoders = nn.Module([
